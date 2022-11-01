14eb4238049ff69d2d7047ec8589259b69e1db4f
delucymartinez <lumartinez84@gmail.com>
2022-11-01 18:46:25 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+addEventListener ("click", totalPrecio)
+//calcular entradas
+
+ function totalPrecio(cantidadTickets, categoriaTickets) {
+       
+if (categoriaTickets == "Estudiante"){
+    document.return(40*cantidadTickets);
+} else if(categoriaTickets == "Trainee"){
+    document.return(100*cantidadTickets);
+} else if(categoriaTickets == "Junior"){
+    document.return(170*cantidadTickets);
+} else if(categoriaTickets == "Sin Categoria"){
+        document.return(1*cantidadTickets);
+}
+}
+
+console.log (totalPrecio)
+ 
